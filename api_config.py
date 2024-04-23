a932299a7d0168cfd8fa45fd196b5c3441c8c401
carlosg20365 <121314260+carlosg20365@users.noreply.github.com>
2024-04-23 00:01:26 -0300

    Create api_config.py

---
+++ api_config.py
@@ -0,0 +1,17 @@
+
+# Put your APIs inside the quotation marks.
+exchange_credentials = {
+    'binance': {'api_key': '', 'api_secret': ''},
+    'kucoin': {'api_key': '', 'api_secret': ''},
+    'lbank': {'api_key': '', 'api_secret': ''},
+    'huobi': {'api_key': '', 'api_secret': ''},
+    'kraken': {'api_key': '', 'api_secret': ''},
+    'phemex': {'api_key': '', 'api_secret': 'y'},
+    'latoken': {'api_key': '', 'api_secret': ''},
+    'bigone': {'api_key': '', 'api_secret': ''},
+    'mexc': {'api_key': '', 'api_secret': ''},
+    'gemini': {'api_key': '', 'api_secret': ''},
+    'tokocrypto': {'api_key': '', 'api_secret': ''},
+    'hitbtc': {'api_key': '', 'api_secret': ''}
+     
+}
